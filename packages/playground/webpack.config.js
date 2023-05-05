8fc87dd69ad11022d215fa533905b161ade12897
kevinjohannesson <kroon.kevin@gmail.com>
2023-05-05 22:56:48 +0200

    added typescript to playground

--- packages/playground/webpack.config.js
+++ packages/playground/webpack.config.js
@@ -1,34 +1,31 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.tsx?$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
   devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
     port: 3000,
     open: true,
-    hot: true,
   },
 };
