af9fef89f743a94515f5c8aed675f8469b897461
kevinjohannesson <kroon.kevin@gmail.com>
2023-05-06 05:19:21 +0200

    added geoviewer and some styling

--- packages/playground/src/app.tsx
+++ packages/playground/src/app.tsx
@@ -1,7 +1,12 @@
-// import { GisViewer } from "gis-viewer";
+import "./app.css";
+import { GisViewer } from "gis-viewer";
 
 const App = () => {
-  return <div>{/* <GisViewer id="default" /> */}</div>;
+  return (
+    <div>
+      <GisViewer id="default" />
+    </div>
+  );
 };
 
 export default App;
